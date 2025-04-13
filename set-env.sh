4d47ba4bb326778840722047fa73ee34a15a9e80
dushyanth96 <dushyanthpragada9@gmail.com>
2025-04-13 07:09:40 +0000

    updated responses

--- set-env.sh
+++ set-env.sh
@@ -1,8 +1,7 @@
 PORT=8000
 
 # AI API Configuration (for future implementation)
-GEMINI_API_KEY=AIzaSyDjlXxSUDqUKVWXMTZ-sveZN2FkrTzCDUc
+GEMINI_API_KEY=${GEMINI_API_KEY}
 GEMINI_API_ENDPOINT=https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent
 
-
-OPENAI_API_KEY=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
+OPENAI_API_KEY=${OPENAI_API_KEY}
